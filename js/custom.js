6db865409305258a1abaf7aa3008d6df0b916355
Daniel Letscher <dpletscher@outlook.com>
2016-06-06 16:42:00 -0500

    initial scrollspy implementation

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$('#nav').affix({
+    offset: {
+        top: $('#nav').offset().top
+    }
+});
+
+$('#nav').affix({
+  offset: {
+    bottom: ($('footer').outerHeight(true) + 
+            $('.application').outerHeight(true)) + 
+            40
+  }
+});
