77eb853d4732cb1b5e80a1132d2adbb2773f6c15
tangdrew <andrewtang@sbcglobal.net>
2016-06-06 15:21:34 -0500

    moved small dataset to data folder

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from io import StringIO
-df = pd.read_csv('yelp_academic_dataset_business_small.csv')
+df = pd.read_csv('./data/yelp_academic_dataset_business_small.csv')
 print(df.head())
 
